693798b7cad7a951473c7300ae2e96b86dfd8452
Alex <renarcell@gmail.com>
2021-07-01 18:14:13 +0300

    +catalog+reviews(html only)

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,16 @@
-$('.carousel-pulse').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    centerMode: true
-  });
+$(document).ready(function() {
+  $('.carousel-pulse').slick({
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="img/4carousel/icons/prevArrow.svg" alt="prevArrow"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="img/4carousel/icons/nextArrow.svg" alt="nextArrow"></button>',
+    });
+
+    $('div.catalog-captions').on('click', 'div.catalog__tab:not(.catalog__tab-active)', function() {
+      $(this)
+        .addClass('catalog__tab-active').siblings().removeClass('catalog__tab-active')
+        .closest('div.catalog-tabs').find('div.catalog-items').removeClass('catalog__tab-active').eq($(this).index()).addClass('catalog__tab-active');
+    });
+})
